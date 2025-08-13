5f7f5596b412cdbe8bbcc496e744f4bcf7e645d8
jjuchan <yjc7241@naver.com>
2025-08-13 21:07:04 +0900

    testPostService를 직접 넣어서 테스트 완료

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,18 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.testPost.service.TestPostService;
+
 public class ApplicationContext {
     public ApplicationContext() {
 
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+
+        if (beanName.equals("testPostService")) {
+            return (T) new TestPostService();
+        }
+        return  null;
     }
+
 }
